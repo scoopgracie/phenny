aab29cb53d00e48a4928e4544025fa6ed4a5072b
Ng Wei En <weien1292+github@gmail.com>
2018-10-28 01:14:30 +0800

    Make several enhancements to the GCI module (#447)
    Improves on https://github.com/apertium/phenny/issues/437

--- modules/gci.py
+++ modules/gci.py
@@ -1,3 +1,4 @@
+import hashlib
 import sqlite3
 import time
 from tools import DatabaseCursor, db_path
@@ -7,27 +8,33 @@ def setup(self):
     # entries exist for as long as nick is active
     self.gci_data = {}
 
-    # persistent data: nick -> full_name, all_time
-    self.gci_db = db_path(self, 'gci')
+    # persistent data: nick -> mentor_nick, code, all_time
+    self.gci_db = db_path(self, 'gci_data')
 
     connection = sqlite3.connect(self.gci_db)
     cursor = connection.cursor()
 
-    cursor.execute('''create table if not exists gci_data (
+    cursor.execute('''CREATE TABLE IF NOT EXISTS gci_data (
         nick        varchar(255),
-        full_name   varchar(255),
+        mentor_nick varchar(255),
+        code        varchar(255),
         all_time    unsigned big int not null default 0,
-        unique (nick, full_name) on conflict replace
+        UNIQUE (nick, code) ON CONFLICT REPLACE
     );''')
 
     cursor.close()
     connection.close()
-
+    
 def teardown(self):
-    # save active nicks to disc
-
+    # save active nicks to disk
     for nick in self.gci_data:
         inactivity(self, nick)
+
+def gen_code(nick, mentor_nick):
+        m = hashlib.sha256()
+        m.update(nick.encode())
+        m.update(mentor_nick.encode())
+        return m.hexdigest()[8:]        
 
 def select(phenny, nick):
     sqlite_data = {
@@ -35,14 +42,14 @@ def select(phenny, nick):
     }
 
     with DatabaseCursor(phenny.gci_db) as cursor:
-        cursor.execute("SELECT full_name, all_time FROM gci_data WHERE nick=:nick", sqlite_data)
+        cursor.execute("SELECT mentor_nick, code, all_time FROM gci_data WHERE nick=:nick", sqlite_data)
         return cursor.fetchone()
 
-def insert(phenny, nick, full_name, all_time=0):
-    values = (nick, full_name, all_time)
+def insert(phenny, nick, mentor_nick, code, all_time=0):
+    sqlite_data = (nick, mentor_nick, code, all_time)
 
     with DatabaseCursor(phenny.gci_db) as cursor:
-        cursor.execute("INSERT INTO gci_data (nick, full_name, all_time) VALUES (?, ?, ?)", values)
+        cursor.execute("INSERT INTO gci_data (nick, mentor_nick, code, all_time) VALUES (?, ?, ?, ?)", sqlite_data)
 
 def update(phenny, nick, all_time):
     sqlite_data = {
@@ -51,7 +58,7 @@ def update(phenny, nick, all_time):
     }
 
     with DatabaseCursor(phenny.gci_db) as cursor:
-        cursor.execute("UPDATE gci_data set all_time=:all_time where nick=:nick", sqlite_data)
+        cursor.execute("UPDATE gci_data SET all_time=:all_time WHERE nick=:nick", sqlite_data)
 
 def delete(phenny, nick):
     sqlite_data = {
@@ -62,23 +69,19 @@ def delete(phenny, nick):
         cursor.execute("DELETE FROM gci_data WHERE nick=:nick", sqlite_data)
 
 def commutate(phenny, nick):
-    # all_time (persistent) += now_time - last_time (volatile)
-    # last_time = now_time
-
     now_time = time.time()
     last_time = phenny.gci_data[nick]['last_time']
     new_time = now_time - last_time
 
-    full_name, all_time = select(phenny, nick)
-    all_time += new_time
+    mentor_nick, code, all_time = select(phenny, nick)
+    all_time += now_time - last_time
 
     update(phenny, nick, all_time)
     phenny.gci_data[nick]["last_time"] = now_time
 
     # notify if all_time crossed 4 hours
-
     if all_time - new_time <= 4*60*60 <= all_time:
-        phenny.msg("#apertium", full_name + " stayed on the IRC channel for four hours.")
+        phenny.msg("#apertium", "{} (linked by {}) has stayed on the IRC channel for four hours! Code: {}".format(nick, mentor_nick, code))
 
 def activity(phenny, nick):
     if nick in phenny.gci_data:
@@ -127,17 +130,19 @@ def messaging(phenny, input):
 
 messaging.rule = r'(.*)'
 
-def linking(phenny, input):
-    nick = input.nick.casefold()
-    full_name = input.group(1)
-
-    if not full_name:
-        phenny.reply("Syntax: .gci <full_name>")
+def linking(phenny, input):  
+    if not input.group(1):
+        phenny.reply("Syntax: .gci <nick>")
         return
+    
+    mentor_nick = input.nick.casefold()
+    nick = input.group(1)
+    code = gen_code(nick, mentor_nick)
 
-    insert(phenny, nick, full_name)
+    insert(phenny, nick, mentor_nick, code)
     activity(phenny, nick)
-
+    phenny.msg("#apertium", "{} has been linked to the GCI module by {}! Code: {}".format(nick, mentor_nick, code))
+    
 linking.rule = r'\.gci(?:\s+(.+))'
 
 def checking(phenny, input):
@@ -146,18 +151,16 @@ def checking(phenny, input):
     if not nick:
         phenny.reply("Syntax: .gci_check <nick>")
         return
+    
+    t = select(phenny, nick)
+    if not t:
+        phenny.reply("The nick '{}' isn't linked.".format(t))
+        return
 
     if nick in phenny.gci_data:
         commutate(phenny, nick)
-
-    row = select(phenny, nick)
-
-    if not row:
-        phenny.reply("This nick isn't linked")
-        return
-
-    full_name, all_time = row
-    phenny.reply("nick: %s, full name: %s, all time: %s / 14400 seconds" % (nick, full_name, int(all_time)))
+    mentor_nick, code, all_time = t
+    phenny.reply("nick: {}, mentor: {}, code: {}, all time: {} s ({:.2f} hrs)" % (nick, mentor_nick, code, int(all_time), int(all_time) / 3600))
 
 checking.rule = r'\.gci_check(?:\s+(.+))'
 
@@ -186,19 +189,19 @@ def nickchange(phenny, input):
     old_nick = input.nick.casefold()
     new_nick = input.args[1].casefold()
 
-    row = select(old_nick)
-
-    if not row:
+    t = select(old_nick)
+    if not t:
         # nick not linked, don't care
         return
-
-    full_name, all_time = row
-
+    mentor_nick, code, all_time = t
+    
     inactivity(phenny, old_nick)
     delete(phenny, old_nick)
 
-    insert(phenny, new_nick, full_name, all_time)
+    insert(phenny, new_nick, mentor_nick, code, all_time)
     activity(phenny, new_nick)
+    
+    phenny.msg("#apertium", "{} (linked to the GCI module by {}) has been changed to {}. Code: {}".format(old_nick, mentor_nick, new_nick, code))
 
 nickchange.event = "NICK"
 nickchange.rule = r'(.*)'
