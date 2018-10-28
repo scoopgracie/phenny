1b26e56f22032d9ecce87260de975255a29d1dfb
luo-yuyuan <yuyuanluo.fhc@gmail.com>
2018-10-28 17:45:33 -0400

    Add tests in test_apertiumchill

--- modules/test/test_apertiumchill.py
+++ modules/test/test_apertiumchill.py
@@ -6,7 +6,16 @@ class TestApertiumchill(unittest.TestCase):
     def setUp(self):
         self.phenny = MagicMock()
         self.input = MagicMock()
+        self.input.sender = "Testsworth"
 
-    def test_chill(self):
+    def test_chill_chill(self):
+        apertiumchill.measure.channels["Testsworth"] = 20
         apertiumchill.chill(self.phenny, self.input)
-        self.assertTrue(self.phenny.say.called)
+        out = self.phenny.say.call_args[0][0]
+        self.assertTrue(out.find("chill level is currently:") != -1)
+
+    def test_chill_notchill(self):
+        apertiumchill.measure.channels["Testsworth"] = 0
+        apertiumchill.chill(self.phenny, self.input)
+        out = self.phenny.say.call_args[0][0]
+        self.assertTrue(out.find("WARNING: CHILL LEVEL IS DANGEROUSLY LOW. RECOMMEND") != -1)
