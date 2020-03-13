69ff9e86cd44b1b1eff2ddd682e666fdd900de58
scoopgracie <scoopgracie@gmail.com>
2020-03-12 19:04:24 -0700

    add .checkup

---
+++ modules/check.py
@@ -0,0 +1,6 @@
+def checkup(phenny, input):
+    """.checkup - Check if begiak is up"""
+    phenny.say("I'm up and running!")
+checkup.commands = ['checkup']
+checkup.priority = 'high'
+checkup.example = '.checkup'
